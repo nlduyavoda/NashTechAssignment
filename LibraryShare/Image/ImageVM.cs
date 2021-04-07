63007ad79dd282b00510c0a03ce99bb27d25fa37
Duy <nguyenleduy1199@gmail.com>
2021-04-07 18:03:13 +0700

    Add ViewModel (image and product)

---
+++ LibraryShare/Image/ImageVM.cs
@@ -0,0 +1,7 @@
+namespace LibraryShare.Image
+{
+  public class ImageVM
+  {
+    public string pathImage { get; set; }
+  }
+}
---
+++ LibraryShare/Product/ProductVM.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using LibraryShare.Image;
+
+namespace LibraryShare.Product
+{
+  public class ProductVM
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string pathImage { get; set; }
+    public double Price { get; set; }
+    public int? CategoryId { get; set; }
+
+    public List<ImageVM> Images { get; set; }
+  }
+}
