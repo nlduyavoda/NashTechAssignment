66f73645baba1d37df2e71ebec576bc60a53ac46
Duy <nguyenleduy1199@gmail.com>
2021-04-07 12:50:21 +0700

    re Set up folder Product

--- client/Controllers/HomeController.cs
+++ client/Controllers/HomeController.cs
@@ -7,39 +7,25 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using client.Models;
 using Microsoft.AspNetCore.Authorization;
-using backend.Reponsitories.ProductReponsitories;
 
 namespace client.Controllers
 {
   public class HomeController : Controller
   {
     private readonly ILogger<HomeController> _logger;
-    private readonly IProduct _product;
-
-    public HomeController(ILogger<HomeController> logger,
-      IProduct product)
+    public HomeController(ILogger<HomeController> logger)
     {
       _logger = logger;
-      _product = product;
     }
-
     public IActionResult Index()
     {
       return View();
     }
-    public async Task<IActionResult> Product()
-    {
-      var product = await _product.GetProductById(1);
-      return View(product);
-    }
-
-
     [Authorize]
     public IActionResult Privacy()
     {
       return View();
     }
-
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
