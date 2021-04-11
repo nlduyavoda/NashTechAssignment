6037099fa784ca17cbb189b8dd769f47177a67f9
Duy <nguyenleduy1199@gmail.com>
2021-04-11 19:34:54 +0700

    Categories_ViewModel_added

--- LibraryShare/Product/ProductVM.cs
+++ LibraryShare/Product/ProductVM.cs
@@ -10,7 +10,6 @@ namespace LibraryShare.Product
     public string pathImage { get; set; }
     public double Price { get; set; }
     public int? CategoryId { get; set; }
-
     public List<ImageVM> Images { get; set; }
   }
 }
---
+++ LibraryShare/Rating/RatingRes.cs
@@ -0,0 +1,10 @@
+namespace LibraryShare.Rating
+{
+  public class RatingRes
+  {
+    public int Id { get; set; }
+    public string Value { get; set; }
+    public string UserId { get; set; }
+    public int ProductId { get; set; }
+  }
+}
