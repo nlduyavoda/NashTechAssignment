43eeb5ecfb74f2c53fcf5d7d39627906ea937789
nlduyavoda <nguyenleduy1199@gmail.com>
2021-04-20 15:43:22 +0700

    RatingModel_editded

--- backend/Models/Rating.cs
+++ backend/Models/Rating.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace backend.Models
 {
-  public class Rating : Autiable
+  public class Rating
   {
     public int Id { get; set; }
     public int Value { get; set; }
