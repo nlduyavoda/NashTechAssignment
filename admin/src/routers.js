62ccc7ce1d8af90b4ddec12c9dbedaac4ca58b1f
nlduyavoda <nguyenleduy1199@gmail.com>
2021-04-27 11:48:07 +0700

    router_auth_oidc

---
+++ admin/src/routers.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Product from "./pages/products/Product";
+import Category from "./pages/categories/Category";
+import User from "./pages/users/User";
+import NotMatch from "./pages/errors/NotMatch";
+//authen
+import SigninOidc from "./pages/auth/Signin-oidc";
+import SignoutOidc from "./pages/auth/Signout-oidc";
+import PrivateRoute from "./utils/protectedRoute";
+
+
+export default function Routes(props) {
+    return (
+        <switch>
+            <Route path="/signout-oidc" component={SignoutOidc} />
+            <Route path="/signin-oidc" component={SigninOidc} />
+
+            <Redirect exact from="/" to="/products" />
+
+            <PrivateRoute path="/products" component={Product} />
+            <PrivateRoute path="/categories" component={Category} />
+            <PrivateRoute path="/users" component={User} />
+
+            <Route path="*" component={NotMatch} />
+        </switch>
+    )
+}
