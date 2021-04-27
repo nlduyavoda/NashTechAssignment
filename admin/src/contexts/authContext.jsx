9fcf3400e66b43b6a54c43e3cb5c15d8838545e1
nlduyavoda <nguyenleduy1199@gmail.com>
2021-04-27 11:45:37 +0700

    AuthContext_oidc

--- admin/src/contexts/authContext.jsx
+++ admin/src/contexts/authContext.jsx
@@ -1,6 +1,42 @@
 import React, { createContext, useState } from 'react';
 import { signinRedirectCallback } from '../Services/authService';
+import {
+    STORE_USER,
+    USER_SIGNED_OUT,
+    USER_EXPIRED,
+    LOADING_USER,
+} from './types';
+import { setAuthHeader } from "../utils/axiosHeaders";
 
+
+export function storeUser(user) {
+    alert(user.access_token);
+    setAuthHeader(user.access_token);
+    return {
+        type: STORE_USER,
+        payload: user,
+    };
+}
+export function loadingUser() {
+    return {
+        type: LOADING_USER,
+    };
+}
+export function storeUserError() {
+    return {
+        type: STORE_USER_ERROR,
+    };
+}
+export function userExpired() {
+    return {
+        type: USER_EXPIRED,
+    };
+}
+export function userSignedOut() {
+    return {
+        type: USER_SIGNED_OUT,
+    };
+}
 export const AuthContext = createContext({});
 
 const AuthContextProvider = ({ children }) => {
