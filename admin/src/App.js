8a919f8b4aa4e74fb2934e3db345512fe56b0187
nlduyavoda <nguyenleduy1199@gmail.com>
2021-04-30 22:03:30 +0700

    app.js_config

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,14 +1,13 @@
-import { Container, Table, Button } from 'reactstrap';
-import { PenFill } from 'react-bootstrap-icons';
 import Header from './components/Header';
 import './App.css';
-import { ProductContext } from './contexts/products';
-import { useContext, useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { BrowserRouter, Route, Link } from "react-router-dom";
-import CartDetail from './Container/CartDetail';
 import Addform from './components/form/product/add';
-import Editform from './components/form/product/edit';
-import Home from './Container/Home';
+import EditProduct from './components/form/product/edit';
+import Home from './components/Product/Home';
+import Category from './components/Category/Category';
+import EditCategory from './components/form/category/edit';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'sweetalert2/src/sweetalert2.scss'
 import ProtecedRoute from "./utils/protectedRoute";
@@ -29,7 +28,6 @@ import { Switch } from 'react-router-dom';
 
 function App() {
   useEffect(() => {
-    // fetch current user from cookies
     loadUserFromStorage(store);
   }, []);
 
@@ -39,20 +37,30 @@ function App() {
         <BrowserRouter>
           <Header />
           <Switch>
-
             <Route path="/signout-oidc" component={SignoutOidc} />
             <Route path="/signin-oidc" component={SigninOidc} />
+
             <ProtecedRoute exact path="/">
               <Home />
             </ProtecedRoute>
-            <Route exact path="/CartDetail">
-              <CartDetail />
-            </Route>
             <Route exact path="/components/form/product/Addform">
               <Addform />
             </Route>
-            <Route path="/components/form/product/Editform" component={Editform}>
+            <Route path="/edit-product" component={EditProduct}>
             </Route>
+
+            <Route exact path="/category">
+              <Category />
+            </Route>
+            <Route exact path="/components/form/category/Addform">
+              <Addform />
+            </Route>
+            <Route path="/edit-category" component={EditCategory}>
+            </Route>
+
+
+
+
           </Switch>
         </BrowserRouter>
       </AuthProvider>
